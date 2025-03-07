731247c5768dfa92c8c9716ae4b9196e0648fae4
FrogIncorporated <johndseng@gmail.com>
2025-03-07 06:58:35 -0500

    Delete getComments.js

--- getComments.js
+++
@@ -1,8 +0,0 @@
-let comments = global.comments || {};
-exports.handler = async function(event, context) {
-  const postId = event.queryStringParameters.postId;
-  return {
-    statusCode: 200,
-    body: JSON.stringify(comments[postId] || [])
-  };
-};
