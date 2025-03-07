9b6f164ad6dd50c24c2432589c0cc34a8ed2b881
FrogIncorporated <johndseng@gmail.com>
2025-03-07 06:57:08 -0500

    Update and rename addComment.js to

--- addComment.js
+++ addComment.js
@@ -1,17 +1,1 @@
-let comments = global.comments || {};
-if (!global.comments) {
-  global.comments = comments;
-}
-exports.handler = async function(event, context) {
-  const data = JSON.parse(event.body);
-  const postId = data.postId;
-  const comment = { text: data.text, timestamp: Date.now() };
-  if (!comments[postId]) {
-    comments[postId] = [];
-  }
-  comments[postId].push(comment);
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: 'Comment added', comment })
-  };
-};
+
