18f68ef820c730d22a91424024e6cfbc912b8f68
FrogIncorporated <johndseng@gmail.com>
2025-03-08 08:23:06 -0500

    Update getPosts.js

--- netlify/functions/getPosts.js
+++ netlify/functions/getPosts.js
@@ -1,7 +1,21 @@
-let posts = global.posts || [];
-exports.handler = async function(event, context) {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(posts)
-  };
+const faunadb = require('faunadb'),
+      q = faunadb.query;
+
+const client = new faunadb.Client({ secret: process.env.FAUNADB_SECRET });
+
+exports.handler = async (event, context) => {
+  try {
+    // Use the index 'all_posts_by_date' you created (with term: data.type, value: data.timestamp descending)
+    const result = await client.query(
+      q.Paginate(q.Match(q.Index('all_posts_by_date')))
+    );
+    // Retrieve full documents for each post
+    const posts = await client.query(
+      q.Map(result.data, ref => q.Get(ref))
+    );
+    return { statusCode: 200, body: JSON.stringify(posts) };
+  } catch (error) {
+    return { statusCode: 400, body: JSON.stringify(error) };
+  }
 };
+
